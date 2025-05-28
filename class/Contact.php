69d3128e45ff71d2e953519454cf4e49010c1f26
pumba250 <158454148+pumba250@users.noreply.github.com>
2025-05-28 13:51:42 +0300

    Update Contact.php
    {$dbPrefix}

--- class/Contact.php
+++ class/Contact.php
@@ -7,11 +7,11 @@ class Contact {
     }
 
     public function saveMessage($name, $email, $message) {
-        $stmt = $this->pdo->prepare("INSERT INTO blogs_contacts (name, email, message) VALUES (?, ?, ?)");
+        $stmt = $this->pdo->prepare("INSERT INTO {$dbPrefix}blogs_contacts (name, email, message) VALUES (?, ?, ?)");
         return $stmt->execute([$name, $email, $message]);
     }
 	public function deleteMessage($id) {
-		$stmt = $this->pdo->prepare("DELETE FROM blogs_contacts WHERE id = ?");
+		$stmt = $this->pdo->prepare("DELETE FROM {$dbPrefix}blogs_contacts WHERE id = ?");
 		return $stmt->execute([$id]);
 	}
 }
