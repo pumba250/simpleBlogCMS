ab4216533215a5f4734771864f817adcb012a3d8
pumba250 <158454148+pumba250@users.noreply.github.com>
2025-06-05 12:44:51 +0300

    Add files via upload

--- class/Template.php
+++ class/Template.php
@@ -33,4 +33,35 @@ class Template {
         $this->templateDir = $dir;
         return $this;
     }
+	function getAvailableTemplates($templatesDir) {
+		$availableTemplates = [];
+		if (is_dir($templatesDir)) {
+			$items = scandir($templatesDir);
+			foreach ($items as $item) {
+				$fullPath = $templatesDir.'/'.$item;
+				if ($item != '.' && $item != '..' && is_dir($fullPath)) {
+					if (file_exists($fullPath.'/footer.tpl') && 
+						file_exists($fullPath.'/header.tpl')) {
+						$availableTemplates[] = $item;
+					}
+				}
+			}
+		}
+		return $availableTemplates;
+	}
+
+	function changeTemplate($newTemplate, $availableTemplates, &$config, $configPath) {
+		if (in_array($newTemplate, $availableTemplates)) {
+			$config['templ'] = $newTemplate;
+			$configContent = "<?php\nreturn " . var_export($config, true) . ";\n";
+			
+			if (file_put_contents($configPath, $configContent, LOCK_EX)) {
+				if (function_exists('opcache_invalidate')) {
+					opcache_invalidate($configPath);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
 }
